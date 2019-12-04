0619bf2c34a1717e64528130e0611c2613034226
Tamara Golub <tgolub89@gmail.com>
2019-12-04 08:55:58 -0500

    added the function to pull a question and the choices.

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,38 @@
-
 var btnStart = document.getElementById("btn-start");
+var questionsDisplay = document.getElementById("questionsDisplay");
 
-btnStart.addEventListener("click", function(){
-    btnStart.style.display = "none";
-})
+questionsDisplay.addEventListener("click", function(e) {
+  if (e.target && e.target.className === "btn-choice") {
 
+    var answer = document.createElement("h6");
+
+    if (e.target.textContent === questions[0].answer) {
+      
+      answer.textContent = "Correct";
+    } else {
+        
+        answer.textContent = "Wrong";
+    }
+    questionsDisplay.appendChild(answer);
+  }
+});
+
+btnStart.addEventListener("click", function() {
+  btnStart.style.display = "none";
+
+  // h3 for the title
+  var h3title = document.createElement("h3");
+  h3title.textContent = questions[0].title;
+  questionsDisplay.appendChild(h3title);
+
+  // buttons for each answer
+
+  for (var i = 0; i < questions[0].choices.length; i++) {
+    var choice = document.createElement("button");
+    choice.textContent = questions[0].choices[i];
+    choice.classList.add("btn-choice");
+    questionsDisplay.appendChild(choice);
+  }
+
+  console.log(questions[0]);
+});
